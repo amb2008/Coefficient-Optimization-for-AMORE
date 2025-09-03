3b015dcb68edd81771a4a4a157f20dc608a6c732
Alex Berman <c26ab1@dalton.org>
2025-09-03 14:00:20 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,1 @@
-config = {'mech_file': './reduced_mechanism_alex_project.txt', 'full_eqn': './caltech_amore_isoprene_full_eqn.txt', 'full_spc': './caltech_amore_isoprene_full_spc.txt', 'input_conditions': 1, 'individual_params': 2}
+config = {'mech_file': './reduced_mech.txt', 'full_eqn': './caltech_amore_isoprene_full_eqn.txt', 'full_spc': './caltech_amore_isoprene_full_spc.txt', 'input_conditions': 1, 'individual_params': 2}
