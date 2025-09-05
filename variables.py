dccd0d3cab882981ad40def4f796d278fd73af1d
Alex Berman <c26ab1@dalton.org>
2025-09-05 14:51:48 -0400

    Add files via upload

--- variables.py
+++ variables.py
@@ -7,4 +7,4 @@
 
 #config = {'mech_file': './reduced_mech.txt', 'full_eqn': './caltech_amore_isoprene_full_eqn.txt', 'full_spc': './caltech_amore_isoprene_full_spc.txt', 'input_conditions': 1, 'individual_params': 2, 'ignore': "ones"}
 
-config = {'mech_file': './reduced_mech.txt', 'full_eqn': './caltech_amore_isoprene_full_eqn.txt', 'full_spc': './caltech_amore_isoprene_full_spc.txt', 'input_conditions': 1, 'individual_params': 2, 'ignore': "ones"}
+config = {'mech_file': './reduced_mech.txt', 'full_eqn': './caltech_amore_isoprene_full_eqn.txt', 'full_spc': './caltech_amore_isoprene_full_spc.txt', 'input_conditions': 1, 'individual_params': 2, 'ignore': "nothing"}
